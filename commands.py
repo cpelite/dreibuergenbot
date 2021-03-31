0b26fae8da38eef5fd132fe656cf264ee36a2e87
cpelite <bene@fell-it.at>
2021-03-31 22:52:03 +0200

    added a fun command

--- commands.py
+++ commands.py
@@ -12,6 +12,12 @@ class funcommands(commands.Cog):
         embed.add_field(name="Delta läuft...", value="...voller Freude, Elan und Enthusiasmus durch den Watschenwald..")
         await ctx.send(embed=embed)
         return
+    @commands.command()
+    async def kaiser(self, ctx):
+        embed = discord.Embed(title="Wichtige Ankündigung")
+        embed.add_field(name="Oh höret alle den Ruf...", value="...lang lebe der Kaiser!")
+        await ctx.send(embed=embed)
+        return
 
 def setup(bot):
     bot.add_cog(funcommands(bot))
