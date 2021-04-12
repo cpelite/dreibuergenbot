79a951ee21831e64009d2dcb414425686f70c6c0
cpelite <bene@fell-it.at>
2021-04-12 21:12:04 +0200

    fixed an error - and this time really.

--- commands.py
+++ commands.py
@@ -27,7 +27,7 @@ class reichslaender(commands.Cog):
     async def reichstal(self, ctx):
         embed = discord.Embed(title="Informationen über Reichstal")
         embed.add_field(name="Hauptstadt", value="Reichstal")
-        embed.add_field(name="Stadtoberhaupt", value="Klaas tom Brok (Schultheiß)")
+        embed.add_field(name="Stadtoberhaupt", value="Klaas tom Brok")
         embed.add_field(name="Regierungschef", value="vakant")
         await ctx.send(embed=embed)
         return
@@ -44,63 +44,63 @@ class reichslaender(commands.Cog):
     @commands.command()
     async def haxagon(self, ctx):
         embed = discord.Embed(title="Informationen über das Königreich Haxagon")
-        embed.add_field(title="Hauptstadt", value="Tuusdorf")
-        embed.add_field(title="Landesoberhaupt", value="Königin Beatrice I.")
-        embed.add_field(title="Regierungschef", value="Stephan Nörden")
+        embed.add_field(name="Hauptstadt", value="Tuusdorf")
+        embed.add_field(name="Landesoberhaupt", value="Königin Beatrice I.")
+        embed.add_field(name="Regierungschef", value="Stephan Nörden")
         await ctx.send(embed=embed)
         return
 
     @commands.command()
     async def rem(self, ctx):
         embed = discord.Embed(title="Informationen über das Königreich Rem")
-        embed.add_field(title="Hauptstadt", value="Montjoie")
-        embed.add_field(title="Landesoberhaupt", value="König Paul-Guillaume III.")
-        embed.add_field(title="Regierungschef", value="Marius de Waldeck-Leon")
+        embed.add_field(name="Hauptstadt", value="Montjoie")
+        embed.add_field(name="Landesoberhaupt", value="König Paul-Guillaume III.")
+        embed.add_field(name="Regierungschef", value="Marius de Waldeck-Leon")
         await ctx.send(embed=embed)
         return
 
     @commands.command()
     async def stauffen(self, ctx):
         embed = discord.Embed(title="Informationen über das Königreich Stauffen")
-        embed.add_field(title="Hauptstadt", value="Hohenstauffen")
-        embed.add_field(title="Landesoberhaupt", value="Xaver von Dunkelstein-Hohenstauffen (Reichskommissar)")
-        embed.add_field(title="Regierungschef", value="Loris von Tanningen")
+        embed.add_field(name="Hauptstadt", value="Hohenstauffen")
+        embed.add_field(name="Landesoberhaupt", value="Xaver von Dunkelstein-Hohenstauffen (Reichskommissar)")
+        embed.add_field(name="Regierungschef", value="Loris von Tanningen")
         await ctx.send(embed=embed)
         return
 
     @commands.command()
     async def hohenburglohe(self, ctx):
         embed = discord.Embed(title="Informationen über das Großherzogtum Hohenburg-Lohe")
-        embed.add_field(title="Hauptstadt", value="Amalien")
-        embed.add_field(title="Landesoberhaupt", value="Friedrich Wilhelm")
-        embed.add_field(title="Regierungschef", value="Nikolaus von Berg")
+        embed.add_field(name="Hauptstadt", value="Amalien")
+        embed.add_field(name="Landesoberhaupt", value="Friedrich Wilhelm")
+        embed.add_field(name="Regierungschef", value="Nikolaus von Berg")
         await ctx.send(embed=embed)
         return
 
     @commands.command()
     async def bazen(self, ctx):
         embed = discord.Embed(title="Informationen über das Großherzogtum Bazen")
-        embed.add_field(title="Hauptstadt", value="Ludwigsruh")
-        embed.add_field(title="Landesoberhaupt", value="Georg III. Rufus (der Unartige)")
-        embed.add_field(title="Regierungschef", value="Elisabeth von Barnim")
+        embed.add_field(name="Hauptstadt", value="Ludwigsruh")
+        embed.add_field(name="Landesoberhaupt", value="Georg III. Rufus (der Unartige)")
+        embed.add_field(name="Regierungschef", value="Elisabeth von Barnim")
         await ctx.send(embed=embed)
         return
 
     @commands.command()
     async def cranach(self, ctx):
         embed = discord.Embed(title="Informationen über das Churfürstenthum Cranach")
-        embed.add_field(title="Hauptstadt", value="Leibach")
-        embed.add_field(title="Landesoberhaupt", value="Constantin I.")
-        embed.add_field(title="Regierungschef", value="Elisabeth Despencer")
+        embed.add_field(name="Hauptstadt", value="Leibach")
+        embed.add_field(name="Landesoberhaupt", value="Constantin I.")
+        embed.add_field(name="Regierungschef", value="Elisabeth Despencer")
         await ctx.send(embed=embed)
         return
 
     @commands.command()
     async def geldern(self, ctx):
         embed = discord.Embed(title="Informationen über das Erzherzogtum Geldern-Veldoril")
-        embed.add_field(title="Hauptstadt", value="Vengard")
-        embed.add_field(title="Landesoberhaupt", value="Ludwig I.")
-        embed.add_field(title="Regierungschef", value="Hagen von Gotha")
+        embed.add_field(name="Hauptstadt", value="Vengard")
+        embed.add_field(name="Landesoberhaupt", value="Ludwig I.")
+        embed.add_field(name="Regierungschef", value="Hagen von Gotha")
         await ctx.send(embed=embed)
         return
 
